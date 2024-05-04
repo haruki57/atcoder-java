21eb5116e16afe36d909ae32cbb912987954a87b
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-05-04 23:16:53 +0900

    ABC352

---
+++ src/main/java/ABC352D.java
@@ -0,0 +1,126 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.TreeSet;
+
+public class ABC352D {
+    static int MOD = 1000000007;
+    static int INF = Integer.MAX_VALUE/2;
+
+    static void run (final FastScanner scanner, final PrintWriter out) {
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int[] a = new int[N];
+        int[] invert = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = scanner.nextInt() - 1;
+            invert[a[i]] = i;
+        }
+        /*
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(invert[i] + " ");
+        }
+        System.out.println();
+
+         */
+        TreeSet<Integer> set = new TreeSet<>();
+        int ans = INF;
+        for (int i = 0; i < K-1; i++) {
+            set.add(invert[i]);
+        }
+        for (int i = K-1; i < N; i++) {
+            set.add(invert[i]);
+            ans = Math.min(ans, Math.abs(set.last()-set.first()));
+            set.remove(invert[i-(K-1)]);
+        }
+        //System.out.println(set);
+        System.out.println(ans);
+    }
+
+    public static void main(final String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner scanner = new FastScanner();
+        try {
+            run(scanner, out);
+        } catch (Throwable e) {
+            throw e;
+        } finally {
+            out.flush();
+        }
+    }
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int buflen = 0;
+        private boolean hasNextByte() {
+            if (ptr < buflen) {
+                return true;
+            }else{
+                ptr = 0;
+                try {
+                    buflen = in.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (buflen <= 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int readByte() { if (hasNextByte()) return buffer[ptr++]; else return -1;}
+        private static boolean isPrintableChar(int c) { return 33 <= c && c <= 126;}
+        public boolean hasNext() { while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++; return hasNextByte();}
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while(isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while(true){
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                }else if(b == -1 || !isPrintableChar(b)){
+                    return minus ? -n : n;
+                }else{
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+        public double nextDouble() { return Double.parseDouble(next());}
+    }
+}
