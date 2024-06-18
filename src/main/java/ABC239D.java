0beca31026c2cf1ded43f3aafe7320281c5843f5
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-06-18 22:55:51 +0900

    まよコン 2024-06-18

---
+++ src/main/java/ABC239D.java
@@ -0,0 +1,119 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class ABC239D {
+    static int MOD = 1000000007;
+    static int INF = Integer.MAX_VALUE/2;
+
+    static void run (final FastScanner scanner, final PrintWriter out) {
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+        boolean[] isPrime = new boolean[1000];
+        Arrays.fill(isPrime, true);
+        isPrime[0]=isPrime[1]=false;
+        for (int i = 1; i *i<= isPrime.length; i++) {
+            if (isPrime[i]) {
+                for (int j = i*2; j < isPrime.length; j+=i) {
+                    isPrime[j]=false;
+                }
+            }
+        }
+        for (int i = A; i <= B; i++) {
+            boolean tWin = true;
+            for (int j = C; j <= D; j++) {
+                if (isPrime[i+j]) {
+                    tWin = false;
+                }
+            }
+            if (tWin) {
+                System.out.println("Takahashi");
+                return;
+            }
+        }
+        System.out.println("Aoki");
+
+    }
+
+    public static void main(final String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner scanner = new FastScanner();
+        try {
+            run(scanner, out);
+        } catch (Throwable e) {
+            throw e;
+        } finally {
+            out.flush();
+        }
+    }
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int buflen = 0;
+        private boolean hasNextByte() {
+            if (ptr < buflen) {
+                return true;
+            }else{
+                ptr = 0;
+                try {
+                    buflen = in.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (buflen <= 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int readByte() { if (hasNextByte()) return buffer[ptr++]; else return -1;}
+        private static boolean isPrintableChar(int c) { return 33 <= c && c <= 126;}
+        public boolean hasNext() { while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++; return hasNextByte();}
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while(isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while(true){
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                }else if(b == -1 || !isPrintableChar(b)){
+                    return minus ? -n : n;
+                }else{
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+        public double nextDouble() { return Double.parseDouble(next());}
+    }
+}
