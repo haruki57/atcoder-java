6aa75e4d5e21ac4a38b065123376386268f35b44
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-06-23 00:30:59 +0900

    ABC359

---
+++ src/main/java/ABC359C.java
@@ -0,0 +1,121 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class ABC359C {
+    static int MOD = 1000000007;
+    static int INF = Integer.MAX_VALUE/2;
+
+    static void run (final FastScanner scanner, final PrintWriter out) {
+        long sx = scanner.nextLong()+10;
+        long sy = scanner.nextLong()+10;
+        long tx = scanner.nextLong()+10;
+        long ty = scanner.nextLong()+10;
+        long diffy = Math.abs(ty-sy);
+        long ans = diffy;
+        if (sy%2==0) {
+            sx = sx - sx%2;
+        } else {
+            sx = sx - (1-sx%2);
+        }
+        if (ty%2==0) {
+            tx = tx - tx%2;
+        } else {
+            tx = tx - (1-tx%2);
+        }
+        //System.out.println(sx + " "+tx);
+        if (sx > tx) {
+            long tmp = sx;
+            sx = tx;
+            tx = tmp;
+        }
+        //System.out.println(sx + " "+tx);
+        sx += diffy;
+        if (sx > tx) {
+            sx = tx;
+        }
+        //System.out.println(sx + " "+tx);
+        System.out.println(ans + (Math.abs(tx-sx)/2));
+
+    }
+
+    public static void main(final String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner scanner = new FastScanner();
+        try {
+            run(scanner, out);
+        } catch (Throwable e) {
+            throw e;
+        } finally {
+            out.flush();
+        }
+    }
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1024];
+        private int ptr = 0;
+        private int buflen = 0;
+        private boolean hasNextByte() {
+            if (ptr < buflen) {
+                return true;
+            }else{
+                ptr = 0;
+                try {
+                    buflen = in.read(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (buflen <= 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private int readByte() { if (hasNextByte()) return buffer[ptr++]; else return -1;}
+        private static boolean isPrintableChar(int c) { return 33 <= c && c <= 126;}
+        public boolean hasNext() { while(hasNextByte() && !isPrintableChar(buffer[ptr])) ptr++; return hasNextByte();}
+        public String next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            StringBuilder sb = new StringBuilder();
+            int b = readByte();
+            while(isPrintableChar(b)) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+        public long nextLong() {
+            if (!hasNext()) throw new NoSuchElementException();
+            long n = 0;
+            boolean minus = false;
+            int b = readByte();
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            if (b < '0' || '9' < b) {
+                throw new NumberFormatException();
+            }
+            while(true){
+                if ('0' <= b && b <= '9') {
+                    n *= 10;
+                    n += b - '0';
+                }else if(b == -1 || !isPrintableChar(b)){
+                    return minus ? -n : n;
+                }else{
+                    throw new NumberFormatException();
+                }
+                b = readByte();
+            }
+        }
+        public int nextInt() {
+            long nl = nextLong();
+            if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE) throw new NumberFormatException();
+            return (int) nl;
+        }
+        public double nextDouble() { return Double.parseDouble(next());}
+    }
+}
